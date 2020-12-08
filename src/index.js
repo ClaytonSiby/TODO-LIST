15b570a3952374b5c2023f9064bfdf801ce69586
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-12-08 18:54:43 +0530

    Add task to localStorage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("hello");
+let defaultProject = [];
+
+function Task(title, description, duedate, notes, priority) {   
+  this.title = title;
+  this.description = description;
+  this.duedate = duedate; 
+  this.notes = notes;
+  this.priority = priority;
+}
+
+let submitTask = document.getElementById('submit-task');
+
+function addTaskToProject(e) {
+  e.preventDefault();
+  let taskTitle = document.getElementById('task-title').value;
+  let taskDescription = document.getElementById('task-description').value;
+  let taskduedate = document.getElementById('task-duedate').value;
+  let taskNotes = document.getElementById('task-notes').value;
+  let taskPriority = document.getElementById('task-priority').value;
+
+  let userTask = new Task(taskTitle, taskDescription, taskduedate, taskNotes, taskPriority);
+  
+  defaultProject.push(userTask);
+
+  localStorage.setItem('myTask', JSON.stringify(defaultProject));
+
+
+  taskTitle.value = '';
+  taskDescription.value = '';
+  taskduedate.value = '';
+  taskNotes.value = '';
+  taskPriority.value = '';
+  
+}
+
+
+submitTask.addEventListener('click', addTaskToProject);
