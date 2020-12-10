3ec739d60e2b579a66623ea6686eeb684ccb8076
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-12-10 14:56:24 +0530

    Save project list to localStorage

--- src/index.js
+++ src/index.js
@@ -1,42 +1,94 @@
-let defaultProject = [];
+// let defaultProject = [];
 
-function Task(title, description, duedate, notes, priority) {   
-  this.title = title;
-  this.description = description;
-  this.duedate = duedate; 
-  this.notes = notes;
-  this.priority = priority;
-}
+// function Task(title, description, duedate, notes, priority) {   
+//   this.title = title;
+//   this.description = description;
+//   this.duedate = duedate; 
+//   this.notes = notes;
+//   this.priority = priority;
+// }
 
-let submitTask = document.getElementById('submit-task');
-let projectName = document.getElementById('project-name').value;
-let submitProject = document.getElementById('submit-project');
+// let submitTask = document.getElementById('submit-task');
+// let projectName = document.getElementById('project-name').value;
+// let submitProject = document.getElementById('submit-project');
 
-function createProject() {
+// function createProject() {
   
-}
+// }
 
-function addTaskToProject(e) {
+// function addTaskToProject(e) {
+//   e.preventDefault();
+//   let taskTitle = document.getElementById('task-title').value;
+//   let taskDescription = document.getElementById('task-description').value;
+//   let taskduedate = document.getElementById('task-duedate').value;
+//   let taskNotes = document.getElementById('task-notes').value;
+//   let taskPriority = document.getElementById('task-priority').value;
+
+//   let userTask = new Task(taskTitle, taskDescription, taskduedate, taskNotes, taskPriority);
+  
+//   defaultProject.push(userTask);
+
+//   localStorage.setItem('myTask', JSON.stringify(defaultProject));
+
+
+//   taskTitle.value = '';
+//   taskDescription.value = '';
+//   taskduedate.value = '';
+//   taskNotes.value = '';
+//   taskPriority.value = '';
+  
+// }
+
+// submitTask.addEventListener('click', addTaskToProject);
+
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+
+newListForm.addEventListener('submit', e => {
   e.preventDefault();
-  let taskTitle = document.getElementById('task-title').value;
-  let taskDescription = document.getElementById('task-description').value;
-  let taskduedate = document.getElementById('task-duedate').value;
-  let taskNotes = document.getElementById('task-notes').value;
-  let taskPriority = document.getElementById('task-priority').value;
-
-  let userTask = new Task(taskTitle, taskDescription, taskduedate, taskNotes, taskPriority);
-  
-  defaultProject.push(userTask);
-
-  localStorage.setItem('myTask', JSON.stringify(defaultProject));
+  const listName = newListInput.value;
+  if (listName == null || listName === '') return;
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+});
 
 
-  taskTitle.value = '';
-  taskDescription.value = '';
-  taskduedate.value = '';
-  taskNotes.value = '';
-  taskPriority.value = '';
-  
+function createList(name) {
+  return { id: Date.now().toString(), name: name, tasks:[] }
 }
 
-submitTask.addEventListener('click', addTaskToProject);
+function saveAndRender() {
+  save();
+  render();
+}
+
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+}
+
+function render() {
+  clearElement(listsContainer);
+  lists.forEach(list => {
+    const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
+    listElement.classList.add("list-name");
+    listElement.innerText = list.name;
+    listsContainer.appendChild(listElement);    
+  });
+}
+
+function clearElement(element) {
+ while(element.firstChild) {
+   element.removeChild(element.firstChild);
+ }
+} 
+
+
+render();
