af24064a9ce3dbf0d7544bcca08a7503634100ba
Clayton Siby <claytonsiby@gmail.com>
2021-01-04 12:02:43 +0200

    Upate test files

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -61,20 +61,3 @@ describe('Testing createTask function', () => {
   })
 })
 
-describe('Create a new list', () => {
-  let id = Date.now().toString()
-  let listName = 'New List'
-  let newList = createList(listName, id)
-
-  test('Successfully create a new list', () => {
-    expect(typeof newList).toBe('object')
-  })
-
-  test('The id of a list should be truthy', () => {
-    expect(newList.id).not.toBeNull()
-  })
-
-  test('Should return the value of the list name', () => {
-    expect(newList.name).toEqual('New List')
-  })
-})
--- src/test/saveandrendertask.test.js
+++ src/test/saveandrendertask.test.js
@@ -1,15 +1,23 @@
-import {
-  saveAndRender,
-  render,
-  lists,
-  selectedListId,
-  createList,
-} from '../modules/saveandrender';
+import { saveAndRender, createList } from '../modules/saveandrender'
 
-jest.mock('../modules/saveandrender');
+describe('Create a new list', () => {
+  let id = Date.now().toString()
+  let listName = 'New List'
+  let newList = createList(listName, id)
 
+  test('Successfully create a new list', () => {
+    expect(typeof newList).toBe('object')
+  })
+
+  test('The id of a list should be truthy', () => {
+    expect(newList.id).not.toBeNull()
+  })
+
+  test('Should return the value of the list name', () => {
+    expect(newList.name).toEqual('New List')
+  })
+})
 
 test('Check the presence of task list', () => {
-  expect(saveAndRender()).not.toBeNull();
-});
-
+  expect(saveAndRender()).not.toBeNull()
+})
